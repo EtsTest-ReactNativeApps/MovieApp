4f86a0bef53723b8ebbd2f97ef64ba233a261f1a
DanielSameh <danielsameh35@gmail.com>
2021-04-01 18:56:14 +0200

    Adding Main Navigation

---
+++ src/navigation/MainNavigation.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const MainNavigation = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default MainNavigation
